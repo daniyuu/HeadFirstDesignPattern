85e9baf7dcd837e081f1f9e689012e17bb941234
daniyuu <daniyuu@163.com>
2015-05-10 15:49:49 +0800

    急切的使用单例 2

--- src/instance/Singleton.java
+++ src/instance/Singleton.java
@@ -9,7 +9,9 @@ package instance;
  * @version 
  */
 public class Singleton {
-	private static Singleton uniqueInstance;
+//	private static Singleton uniqueInstance;
+	// 在静态初始化器中创建单例，这段代码保证了线程安全
+	private static Singleton uniqueInstance = new Singleton();
 	/**
 	 * 构造器声明为私有的，只有singleton类内部才可以调用构造器
 	 */
@@ -18,12 +20,16 @@ public class Singleton {
 	}
 	
 	// 通过增加synchronized关键字到getinstance方法中，破事每个线程在进入这个方法之前买药先等候别的线程离开这个方法
+//	public static synchronized Singleton getInstance(){
+//		if(uniqueInstance == null){
+//			uniqueInstance = new Singleton();
+//		}
+//		
+//		return uniqueInstance;
+//	}
+
+	// 应用程序总是创建并使用单例模式，或者在创建和运行时方面的负担不太繁重 急切的想要创建此单间
 	public static synchronized Singleton getInstance(){
-		if(uniqueInstance == null){
-			uniqueInstance = new Singleton();
-		}
-		
 		return uniqueInstance;
 	}
-
 }
